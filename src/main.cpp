45d9651314362f5c8a13838a7eaa90f7524a2665
Tathagata16 <tathagataghosh1609@gmail.com>
2026-08-01 19:50:08 +0000

    initial configuration

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "=============================\n";
+    std::cout << "       MyRedis Server\n";
+    std::cout << "=============================\n";
+    std::cout << "Server starting...\n";
+
+    return 0;
+}
