a53493267bb0b069957d049fec60726392b44c7a
Tathagata16 <tathagataghosh1609@gmail.com>
2026-08-02 08:23:34 +0000

    creating a socket

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 
+//linux networking headers
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
 int main()
 {
     std::cout << "=============================\n";
     std::cout << "       MyRedis Server\n";
     std::cout << "=============================\n";
-    std::cout << "Server starting...\n";
+    
+    //creating a tcp socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(serverSocket == -1){
+        std::cerr<<"Failed to create socket.\n";
+        return 1;
+    }
+
+    std::cout<<"Socket created successfully\n";
+    std::cout<<"Socket Descriptor: " << serverSocket << "\n";
+
+    //close before exit
+    close(serverSocket);
 
     return 0;
 }
